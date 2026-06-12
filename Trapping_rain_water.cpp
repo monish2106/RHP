32eed1579cb6996dcaf486a6f865927c4040e46f
Monish M <monishmageshm@gmail.com>
2026-06-12 16:03:28 +0530

    Update Trapping_rain_water.cpp

--- Trapping_rain_water.cpp
+++ Trapping_rain_water.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> h(n);
+    int h[n];
     for (int i = 0; i < n; i++) {
         cin >> h[i];
     }
-    int l = 0;
-    int r = n - 1;
-    int lm = 0;
-    int rm = 0;
-    int ans = 0;
-    while (l < r) {
-        if (h[l] < h[r]) {
-            if (h[l] >= lm)
-                lm = h[l];
-            else
-                ans += lm - h[l];
-            l++;
-        }
-        else {
-
-            if (h[r] >= rm)
-                rm = h[r];
-            else
-                ans += rm - h[r];
-            r--;
-        }
+    int l[n];
+    int r[n];
+    int m = 0;
+    for (int i = 0; i < n; i++) {
+        if (h[i] > m)
+            m = h[i];
+        l[i] = m;
     }
-    cout << ans;
-
+    m = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (h[i] > m)
+            m = h[i];
+        r[i] = m;
+    }
+    int a = 0;
+    for (int i = 0; i < n; i++) {
+        int w;
+        if (l[i] < r[i])
+            w = l[i];
+        else
+            w = r[i];
+        a += w - h[i];
+    }
+    cout << a;
     return 0;
 }
