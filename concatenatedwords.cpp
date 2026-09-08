74bdadb42a22e0f3e63f789ee843e04787001e47
Monish M <monishmageshm@gmail.com>
2026-09-08 09:00:07 +0530

    Add solution for finding concatenated words
    Implement a solution to find all concatenated words in a dictionary using dynamic programming.

---
+++ concatenatedwords.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> uwords;
+        for(string w:words) uwords.insert(w);
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            int N=words[i].length();
+            int *dp=new int[N+1];
+            dp[0]=1;
+            for(int si=0;si<N;si++){
+                if(dp[si]!=1) continue; 
+                for(int ei=si;ei<N;ei++){
+                    if(ei-si+1 < N && uwords.count(words[i].substr(si,ei-si+1))){
+                        dp[ei+1]=1;
+                    }
+                }
+            }
+            if(dp[N]==1){
+                ans.push_back(words[i]);
+            }
+        }
+        return ans;
+    }
+};
