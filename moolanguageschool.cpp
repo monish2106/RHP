18d3a25905212a98863ab98f5328b48ea2aafa5a
Monish M <monishmageshm@gmail.com>
2026-09-09 09:16:15 +0530

    Implement language school problem solution

---
+++ moolanguageschool.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+void solve(){
+    int n,k; cin>>n>>k;
+    string s; cin>>s;
+    int f=n/k;
+    vector<bool> own(f,false);
+    for(unsigned int i=0;i<s.size();i++){
+        if(s[i]=='0'){
+            int farmidx=i/k;
+            own[farmidx]=true;
+        }
+    }
+    int count=0;
+    for(int F=0;F<f;F++){
+        if(!own[F]) count++;
+    }
+    cout<<count<<endl;
+}
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
